b5a6e3d9f77c8f04727ec92265883018a27365af
duskysabs <21100903@usc.edu.ph>
2026-04-11 18:23:20 +0800

    Added signup and login under the auth module. Additionally added a package.json. Edited the .env file to the supabase we'll use for the project.

--- App/fda-backend/modules/Auth/auth.controller.js
+++ App/fda-backend/modules/Auth/auth.controller.js
@@ -1,4 +1,4 @@
-import { createUserProfile } from "./auth.service.js";
+import { createUserProfile, signupUser, loginUser } from "./auth.service.js";
 
 export const createProfile = async (req, res) => {
   const user = req.user; // from middleware
@@ -8,3 +8,23 @@ export const createProfile = async (req, res) => {
 
   res.json(profile);
 };
+
+export const signup = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const data = await signupUser(email, password);
+    res.json({ message: 'User signed up successfully', data });
+  } catch (error) {
+    res.status(500).json({ error: error.message });   
+  }
+}
+
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const data = await loginUser(email, password);
+    res.json({ message: 'User logged in successfully', data });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+}
--- App/fda-backend/modules/Auth/auth.routes.js
+++ App/fda-backend/modules/Auth/auth.routes.js
@@ -1,9 +1,11 @@
 import express from "express";
-import { createProfile } from "./auth.controller.js";
+import { createProfile, signup, login } from "./auth.controller.js";
 import { authenticate } from "../../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
 router.post("/profile", authenticate, createProfile);
+router.post("/signup", signup);
+router.post("/login", login);
 
 export default router;
