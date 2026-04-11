8f880cfaf2faceb796614790e089fb4da5f8d239
20010109 <20010109@usc.edu.ph>
2026-04-11 20:43:24 +0800

    changes made, having trouble with branch

--- App/fda-backend/server.js
+++ App/fda-backend/server.js
@@ -1,21 +1,24 @@
-import dotenv from "dotenv";
+import 'dotenv/config';
+console.log('SUPABASE_URL:', process.env.SUPABASE_URL);
+import express from "express";
 import cors from "cors";
+import { authenticate } from "./middleware/authMiddleware.js";
+import { createProfile } from "./modules/Auth/auth.controller.js";
 
-dotenv.config();
 
 const app = express();
 
-// middleware
 app.use(cors());
 app.use(express.json());
 
-// test route
 app.get("/", (req, res) => {
-    res.send("API is running...");
-  });
-  
-// start server
+  res.send("API is running...");
+});
+
+// Protected route to create a user profile
+app.post("/api/auth/profile", authenticate, createProfile);
+
 const PORT = process.env.PORT || 3000;
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
