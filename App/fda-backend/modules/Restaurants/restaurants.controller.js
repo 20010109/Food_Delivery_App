ab88af7eada24790b4522a54dad7e274ea847cff
20010109 <20010109@usc.edu.ph>
2026-04-14 23:58:45 +0800

    added is_approval on documentation for database config, working on restaurants module

---
+++ App/fda-backend/modules/Restaurants/restaurants.controller.js
@@ -0,0 +1,44 @@
+import * as service from "./restaurants.service.js";
+
+export const createRestaurant = async (req, res) => {
+  try {
+    const restaurant = await service.createRestaurant(
+      req.user.id,
+      req.body
+    );
+    res.status(201).json(restaurant);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+export const getMyRestaurants = async (req, res) => {
+  try {
+    const data = await service.getRestaurantsByOwner(req.user.id);
+    res.json(data);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+export const updateRestaurant = async (req, res) => {
+  try {
+    const updated = await service.updateRestaurant(
+      req.params.id,
+      req.user.id,
+      req.body
+    );
+    res.json(updated);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+export const deleteRestaurant = async (req, res) => {
+  try {
+    await service.deleteRestaurant(req.params.id, req.user.id);
+    res.json({ message: "Deleted successfully" });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
