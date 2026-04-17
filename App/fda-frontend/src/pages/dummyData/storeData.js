ddef70ec30b59931c58484ce2e3bbc6bdebdeccd
guzandrenoel <guzandrenoel@gmail.com>
2026-04-17 23:17:46 +0800

    Update store and menu dummy data

--- App/fda-frontend/src/pages/dummyData/storeData.js
+++ App/fda-frontend/src/pages/dummyData/storeData.js
@@ -5,7 +5,8 @@ export const storeData = [
     contact_info: "512-3341",
     image_url:
       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqHutQ-GEKZlgbk4wwnLbEVkbTlrVOWugesg&s",
-    banner_url: "https://source.unsplash.com/1600x500/?burger,fries,fastfood",
+    banner_url:
+      "https://d3.harvard.edu/platform-digit/wp-content/uploads/sites/2/2018/04/brandbanner-mcd-1100x200.png",
     address_id: 1,
     created_at: "2024-06-02T12:30:00Z",
   },
@@ -15,7 +16,8 @@ export const storeData = [
     contact_info: "512-3341",
     image_url:
       "https://upload.wikimedia.org/wikipedia/en/thumb/8/84/Jollibee_2011_logo.svg/500px-Jollibee_2011_logo.svg.png",
-    banner_url: "https://source.unsplash.com/1600x500/?fried-chicken,fastfood",
+    banner_url:
+      "https://images.1851franchise.com/544x306/filters:format(webp)/story1/2729653/1750283235_2729653.png",
     address_id: 3,
     created_at: "2024-06-02T12:30:00Z",
   },
@@ -25,7 +27,8 @@ export const storeData = [
     contact_info: "533-2290",
     image_url:
       "https://upload.wikimedia.org/wikipedia/en/thumb/5/57/KFC_logo-image.svg/250px-KFC_logo-image.svg.png",
-    banner_url: "https://source.unsplash.com/1600x500/?fried-chicken,bucket",
+    banner_url:
+      "https://pimwp.s3-accelerate.amazonaws.com/2021/12/Screenshot-2021-12-28-132323.jpg",
     address_id: 2,
     created_at: "2024-06-03T10:15:00Z",
   },
@@ -34,7 +37,8 @@ export const storeData = [
     name: "Mang Inasal",
     contact_info: "545-8892",
     image_url: "https://upload.wikimedia.org/wikipedia/commons/d/d0/Mang_Inasal.svg",
-    banner_url: "https://source.unsplash.com/1600x500/?grilled-chicken,rice,bbq",
+    banner_url:
+      "https://gadaboutprincess.com/wp-content/uploads/2022/09/Ihaw-Sarap-Banner-1920x1008.jpg",
     address_id: 4,
     created_at: "2024-06-04T09:45:00Z",
   },
@@ -44,7 +48,8 @@ export const storeData = [
     contact_info: "578-1123",
     image_url:
       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ0ybcmWVPS0MpvWDjS3mdT0KuDIsBHvmq_IA&s",
-    banner_url: "https://source.unsplash.com/1600x500/?noodles,dumplings,asian-food",
+    banner_url:
+      "https://www.chowking.ph/wp-content/uploads/2023/11/Share-Image.png?q=80&w=1200&h=630",
     address_id: 5,
     created_at: "2024-06-05T14:20:00Z",
   },
@@ -54,7 +59,8 @@ export const storeData = [
     contact_info: "590-4432",
     image_url:
       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRzQ-yc85xJoNMktdIloK4hK_EShvAv3OVi9g&s",
-    banner_url: "https://source.unsplash.com/1600x500/?pizza,pasta,italian-food",
+    banner_url:
+      "https://www.facebook.com/photo/?fbid=1289712639854953&set=a.653852580107632",
     address_id: 6,
     created_at: "2024-06-06T11:10:00Z",
   },
@@ -64,7 +70,8 @@ export const storeData = [
     contact_info: "601-7788",
     image_url:
       "https://upload.wikimedia.org/wikipedia/commons/thumb/0/06/Pizza_hut_logo_international.svg/960px-Pizza_hut_logo_international.svg.png",
-    banner_url: "https://source.unsplash.com/1600x500/?pizza,pepperoni",
+    banner_url:
+      "https://commons.wikimedia.org/wiki/Special:FilePath/Pizza_Hut_(SM_City_BF_Parañaque_branch)_storefront.jpg?width=1600",
     address_id: 7,
     created_at: "2024-06-07T16:25:00Z",
   },
@@ -73,7 +80,8 @@ export const storeData = [
     name: "Burger King",
     contact_info: "612-9901",
     image_url: "https://upload.wikimedia.org/wikipedia/commons/c/cc/Burger_King_2020.svg",
-    banner_url: "https://source.unsplash.com/1600x500/?burger,cheeseburger",
+    banner_url:
+      "https://commons.wikimedia.org/wiki/Special:FilePath/Burger_King_Festival_Supermall_exterior.jpg?width=1600",
     address_id: 8,
     created_at: "2024-06-08T13:50:00Z",
   },
@@ -83,36 +91,41 @@ export const storeData = [
     contact_info: "623-4455",
     image_url:
       "https://static.wikia.nocookie.net/logopedia/images/a/a6/ArmyNavy_2009_stacked.png/revision/latest/scale-to-width-down/1200?cb=20240224064642",
-    banner_url: "https://source.unsplash.com/1600x500/?burger,sandwich,fries",
+    banner_url:
+      "https://commons.wikimedia.org/wiki/Special:FilePath/Jf7639Army_Navy_Burger_Burritofvf_02.JPG?width=1600",
     address_id: 9,
     created_at: "2024-06-09T08:30:00Z",
   },
   {
     restaurant_id: 10,
-    name: "Shakey’s",
+    name: "Shakey's",
     contact_info: "634-2233",
     image_url:
       "https://upload.wikimedia.org/wikipedia/en/thumb/6/6d/Shakey%27s_US_logo.svg/1280px-Shakey%27s_US_logo.svg.png",
-    banner_url: "https://source.unsplash.com/1600x500/?pizza,chicken,mojito",
+    banner_url:
+      "https://commons.wikimedia.org/wiki/Special:FilePath/Shakey%27s_Festival_Supermall_storefront.jpg?width=1600",
     address_id: 10,
     created_at: "2024-06-10T18:00:00Z",
   },
   {
-  restaurant_id: 11,
-  name: "Domino's Pizza",
-  contact_info: "655-3321",
-  image_url: "https://upload.wikimedia.org/wikipedia/commons/7/74/Dominos_pizza_logo.svg",
-  banner_url: "https://source.unsplash.com/1600x500/?dominos,pizza",
-  address_id: 11,
-  created_at: "2024-06-11T10:00:00Z",
-},
-{
-  restaurant_id: 12,
-  name: "Starbucks",
-  contact_info: "677-8890",
-  image_url: "https://upload.wikimedia.org/wikipedia/sco/thumb/4/45/Starbucks_Corporation_Logo_2011.svg/800px-Starbucks_Corporation_Logo_2011.svg.png",
-  banner_url: "https://source.unsplash.com/1600x500/?coffee,starbucks,cafe",
-  address_id: 12,
-  created_at: "2024-06-12T09:00:00Z",
-}
+    restaurant_id: 11,
+    name: "Domino's Pizza",
+    contact_info: "655-3321",
+    image_url: "https://upload.wikimedia.org/wikipedia/commons/7/74/Dominos_pizza_logo.svg",
+    banner_url:
+      "https://commons.wikimedia.org/wiki/Special:FilePath/Domino%27s_Pizza,_SM_Hypermarket_Sucat_Lopez.jpg?width=1600",
+    address_id: 11,
+    created_at: "2024-06-11T10:00:00Z",
+  },
+  {
+    restaurant_id: 12,
+    name: "Starbucks",
+    contact_info: "677-8890",
+    image_url:
+      "https://upload.wikimedia.org/wikipedia/sco/thumb/4/45/Starbucks_Corporation_Logo_2011.svg/800px-Starbucks_Corporation_Logo_2011.svg.png",
+    banner_url:
+      "https://commons.wikimedia.org/wiki/Special:FilePath/Starbucks_Coffee_Festival_Supermall_storefront.jpg?width=1600",
+    address_id: 12,
+    created_at: "2024-06-12T09:00:00Z",
+  },
 ];
